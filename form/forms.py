268a04f90bcefb1934b8cf11c99972556c982d41
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-07-30 21:21:51 -0600

    added stylings to name and bio inputs

--- form/forms.py
+++ form/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 from .models import Profile, Link
 
+class CustomFileInput(forms.ClearableFileInput):
+    template_name = 'form/widgets/custom_file_input.html' 
+
 class ProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=CustomFileInput(), required=False)
+
     class Meta:
         model = Profile
-        fields = ['avatar', 'name', 'bio']
+        fields = ['avatar', 'name', 'biography']
 
 class LinkForm(forms.ModelForm):
     class Meta:
--- form/models.py
+++ form/models.py
@@ -5,19 +5,19 @@ from utils.file_utils import avatar_upload_to
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=127)
-    bio = models.TextField(blank=True)
+    biography = models.TextField(blank=True)
     avatar = models.ImageField(upload_to=avatar_upload_to, blank=True)
 
     def __str__(self):
         return self.user.username
 
 class Link(models.Model):
-    profile = models.ForeignKey(Profile, related_name='links', on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, related_name="links", on_delete=models.CASCADE)
     label = models.CharField(max_length=127)
     url = models.URLField()
 
     class Meta:
-        ordering = ['id']
+        ordering = ["id"]
 
     def __str__(self):
         return f"{self.label} ({self.url})"
