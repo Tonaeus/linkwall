af92037146e5bede2735ef4825ea570b0bf2a9fb
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-08-02 22:33:59 -0600

    stylized the delete image checkbox

--- form/models.py
+++ form/models.py
@@ -4,9 +4,9 @@ from utils.file_utils import avatar_upload_to
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    avatar = models.ImageField(upload_to=avatar_upload_to, blank=True)
     name = models.CharField(max_length=127)
     biography = models.TextField(blank=True)
-    avatar = models.ImageField(upload_to=avatar_upload_to, blank=True)
 
     def __str__(self):
         return self.user.username
