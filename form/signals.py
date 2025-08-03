742f4fa35dbae7cfda34c649b1c56b933b6e5b17
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-08-03 11:33:41 -0600

    fixed the delete button going on top of the navbar

--- form/signals.py
+++ form/signals.py
@@ -4,8 +4,7 @@ from django.dispatch import receiver
 from .models import Profile
 
 @receiver(post_delete, sender=Profile)
-def delete_avatar_on_delete(sender, instance, **kwargs):
-    print("test")
+def delete_avatar_on_delete_profile(sender, instance, **kwargs):
     if instance.avatar and instance.avatar.path:
         if os.path.isfile(instance.avatar.path):
             os.remove(instance.avatar.path)
