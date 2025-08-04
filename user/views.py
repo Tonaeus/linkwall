2c741a3237fb580d9016b1fbb2c3e6659b1812c2
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-08-03 22:29:27 -0600

    created a separate base file for the navbar

--- user/views.py
+++ user/views.py
@@ -3,7 +3,9 @@ from form.models import Profile
 
 def user_profile(request, slug):
     profile = get_object_or_404(Profile, slug=slug)
+    links = profile.links.all()
 
     return render(request, "user/user_profile.html", {
         "profile": profile,
+        "links": links,
     })
