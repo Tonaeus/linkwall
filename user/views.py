047fbf434065f530fbc05b1d438da7f32764c8d2
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-08-04 01:19:44 -0600

    cleaned up code

--- user/views.py
+++ user/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from form.models import Profile
-from django.forms.models import model_to_dict
+# from django.forms.models import model_to_dict
 
 def user_profile(request, slug):
     profile = get_object_or_404(Profile, slug=slug)
     links = [link for link in profile.links.all() if link.label and link.url]
 
-    print(model_to_dict(profile))
+    # print(model_to_dict(profile))
 
     return render(request, "user/user_profile.html", {
         "profile": profile,
